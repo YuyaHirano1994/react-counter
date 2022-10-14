ec09dab31d93e68714b8657cccc89a1fecea9f03
Yuya Hirano <v322sk@gmail.com>
2022-10-13 17:21:43 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  //count up down
+  const increase = () => {
+    setCounter((count) => count + 1);
+  };
+
+  const increase100 = () => {
+    setCounter((count) => count + 100);
+  };
+
+  const decrease = () => {
+    if (counter > 0) {
+      setCounter((count) => count - 1);
+    }
+  };
+
+  const decrease100 = () => {
+    if (counter > 0 && counter < 100) {
+      setCounter(0);
+    } else if (counter > 0 && counter >= 100) {
+      setCounter((count) => count - 100);
+    }
+  };
+
+  const reset = () => {
+    setCounter(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="counter">
+      <h2>counter</h2>
+      <span className="counter_output">{counter}</span>
+      <div className="btns">
+        <button className="plus" onClick={increase}>
+          +
+        </button>
+        <button className="plus" onClick={increase100}>
+          +100
+        </button>
+        <button className="minus" onClick={decrease}>
+          -
+        </button>
+        <button className="minus" onClick={decrease100}>
+          -100
+        </button>
+        <button className="reset" onClick={reset}>
+          reset
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
